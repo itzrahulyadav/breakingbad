336885597cfd9e53d6ef38790cdb32e8b989a07b
itzrahulyadav <rahulkyadav9691@gmail.com>
2021-12-26 23:20:12 +0530

    adding first time

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../Img/logo.png';
+
+const Header = () => {
+    return (
+        <header className = "center">
+           <img src = {logo} alt = "background" /> 
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import spinner from '../Img/spinner.gif';
+const Spinner = () => {
+    return (
+        <img src={spinner} alt="loading"
+            style={{ width: '200px', margin: 'auto', display: 'block' }}
+        />
+    )
+}
+
+export default Spinner
